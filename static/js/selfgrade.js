332c696707343e016aef38d23903787f778cd08f
Paul Resnick <presnick@umich.edu>
2019-01-06 14:29:47 -0500

    add selfgrade.js file

--- static/js/selfgrade.js
+++ static/js/selfgrade.js
@@ -1,11 +1,12 @@
 function selfGrade(assignment_id, student_id) {
-    var enforceDeadline = false     // for now, don't enforce assignment deadlines for self-grading in Coursera MOOC; should be taken from config
+    var enforceDeadline = false     // don't enforce assignment deadlines for self-grading in Coursera MOOC; should be taken from config
     jQuery.ajax({
         url: eBookConfig.autogradingURL,
         type: "POST",
         dataType: "JSON",
         data: {
             assignment_id: assignment_id,
+            question: null,
             sid: student_id,
             enforceDeadline: enforceDeadline
         },
